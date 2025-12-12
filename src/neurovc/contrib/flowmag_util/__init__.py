84c03a48a828fb1243ded97a650dec9e915ba120
Philipp Flotho <PhilippFlotho@gmail.com>
2025-12-12 14:04:35 +0900

    added a gdown-based model downloader for the flowmag models

---
+++ src/neurovc/contrib/flowmag_util/__init__.py
@@ -0,0 +1,13 @@
+"""Utility helpers for FlowMag."""
+
+from neurovc.contrib.flowmag_util.models import (
+    download_all_flowmag_models,
+    download_flowmag_model,
+    flowmag_model_downloader,
+)
+
+__all__ = [
+    "download_all_flowmag_models",
+    "download_flowmag_model",
+    "flowmag_model_downloader",
+]
