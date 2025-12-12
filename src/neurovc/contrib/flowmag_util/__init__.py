a2d51c8265d26b2a02c70223a0373cf3b49b41c8
Philipp Flotho <PhilippFlotho@gmail.com>
2025-12-12 15:21:23 +0900

    added an online flowmag module compatible with the momag.flow_processing api

--- src/neurovc/contrib/flowmag_util/__init__.py
+++ src/neurovc/contrib/flowmag_util/__init__.py
@@ -5,9 +5,25 @@ from neurovc.contrib.flowmag_util.models import (
     download_flowmag_model,
     flowmag_model_downloader,
 )
+from neurovc.contrib.flowmag_util.online import (
+    FlowMagOnline,
+    FlowMagTTA,
+    OnlineMagnifier,
+    TTAStep,
+    default_alpha_policy,
+    load_flowmag_model,
+    unwrap,
+)
 
 __all__ = [
     "download_all_flowmag_models",
     "download_flowmag_model",
     "flowmag_model_downloader",
+    "FlowMagOnline",
+    "FlowMagTTA",
+    "OnlineMagnifier",
+    "TTAStep",
+    "default_alpha_policy",
+    "load_flowmag_model",
+    "unwrap",
 ]
